f95fa35e2c14abec286d88847b9d9aeb0815e23d
Mark Aiken <gwmaiken@att.net>
2015-04-14 01:44:07 -0400

    Midway through redoing file input

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,13 +3,33 @@ console.log('\'Allo \'Allo!');
 
 var reader = new FileReader();
 
+$('#fileBox').fileinput({
+  uploadUrl: 'parseFile()',
+  showPreview: false
+})
+
+
+
+$('#fileBox').submit( function(e) {
+  console.log('Uploaded')
+});
+
+
 $('#fileBox').change( function(files) {
   var fileList = this.files; /* now you can work with the file list */
   console.log(fileList[0]);
   reader.readAsText(fileList[0]);
 });
 
+
+
 var txt;
+
+$('.fileinput-upload-button').click(function (e) {
+  $()
+  console.log('you');
+});
+
 
 reader.onload = (function(txt) {
   return function(e){
